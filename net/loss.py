0860e18bc95e218b2eab5d262d431c7ade1c711a
WenhaoWang <53076320+WangWenhao0716@users.noreply.github.com>
2020-03-02 18:48:55 +0800

    Update loss.py

--- net/loss.py
+++ net/loss.py
@@ -27,7 +27,6 @@ class cls_pos(nn.Module):
 class reg_pos(nn.Module):
     def __init__(self):
         super(reg_pos, self).__init__()
-        #self.smoothl1 = nn.SmoothL1Loss(reduction='none')
         self.l1 = nn.L1Loss(reduce=False, size_average=False)
     def forward(self, h_pred, h_label):
         l1_loss = h_label[:, 1, :, :]*self.l1(h_pred[:, 0, :, :]/(h_label[:, 0, :, :]+1e-10),
